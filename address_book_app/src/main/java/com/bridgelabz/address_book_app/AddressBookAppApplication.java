8f5efe09693d8c357f2d90f02c3012cd3d7b8fe7
Vivekpatel <vp20032020@gmail.com>
2025-03-01 12:36:22 +0530

    [Vivek] Added UC1

---
+++ address_book_app/src/main/java/com/bridgelabz/address_book_app/AddressBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.address_book_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookAppApplication.class, args);
+	}
+
+}
---
+++ address_book_app/src/test/java/com/bridgelabz/address_book_app/AddressBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.address_book_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
